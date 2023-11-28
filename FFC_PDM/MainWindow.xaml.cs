749291bdf0a47d746b7970065def3be1995ff6fd
chorysmind <raysmc517@naver.com>
2023-11-28 19:43:38 +0900

    콤보박스 데이터 받기 완료했습니다.

--- FFC_PDM/MainWindow.xaml.cs
+++ FFC_PDM/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace FFC_PDM
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 화면을 킬 때 나오는 것
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +30,7 @@ namespace FFC_PDM
             Col1View();
             Col2View();
             Col3View();
+            GenerateCBModelName();
         }
 
         public void GridDataView()
@@ -65,6 +67,50 @@ namespace FFC_PDM
             Dictionary<string, int>  date = new StatisticsTabChartData().ErrorRateData();
             col3 = facilityDataChartControl.CreateBarChart(col3, date, "오류 횟수", true);
             col3.Refresh();
+        }
+
+        public void GenerateCBModelName()
+        {
+            SearchDataFilterl searchDataFilterl = new SearchDataFilterl();
+            CB_ModelName = searchDataFilterl.MadeComboBox(CB_ModelName);
+            
+        }
+        /*
+        public void GenerateCBModelID()
+        {
+            SearchDataFilterl searchDataFilterl = new SearchDataFilterl();
+            CB_Model_ID = searchDataFilterl.MadeIDComboBox(CB_ModelName);
+        }
+        */
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            CB_Model_ID.Items.Clear();
+            //MessageBox.Show("작동함?");
+            // 두번째 콤보박스에 1 2 3 4 아이템 추가하기
+            // 1. 함수에서 리스트를 리턴하고, 리턴된 리스트를 받아서 메시지박스로 출력하는 테스트
+            SearchDataFilterl sdf = new SearchDataFilterl();
+            Dictionary<string, List<double>> machines;
+            List<double> Id_list;
+            /* 새로운 리스트 machineIDList를 생성해서 GetValues()에서 리턴으로 넘어오는 리스트를 받기 */
+            machines = sdf.MadeIDComboBox();
+            machines[CB_ModelName.SelectedItem.ToString()].Sort();
+            Id_list = machines[CB_ModelName.SelectedItem.ToString()];
+            foreach (double i in machines[CB_ModelName.SelectedItem.ToString()])
+            {
+
+                CB_Model_ID.Items.Add((i).ToString());
+            }
+       
+
+            
+            // machineIDList를 콤보박스에 추가하기
+        }
+
+        private void CB_Model_ID_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
         }
     }
 
