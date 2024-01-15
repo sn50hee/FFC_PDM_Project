55275c37e6d41621f0a7b263bf5f290b35612f75
chorysmind <raysmc517@naver.com>
2024-01-15 22:35:01 +0900

    관리자

---
+++ FFC_PDM/PLC_FaceDetection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XGCommLib;
+
+namespace FFC_PDM
+{
+    internal class PLC_FaceDetection
+    {
+        public CommObject20 oCommDriver = null;
+        public Int32 m_nLastCommTime = 0;
+
+        public void Connect()
+        {
+            CommObjectFactory20 factory = new CommObjectFactory20();
+            this.oCommDriver = factory.GetMLDPCommObject20("192.168.1.2:2004");
+
+
+        }
+    }
+}
